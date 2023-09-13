52f658b4ae1a744edaa5ead0bcfc3a061af740df
Jean CREC'HRIOU <jcrechriou@mint.eco>
2023-09-13 09:13:26 +0100

    first commit

---
+++ Server/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+//IPAddress ip = IPAddress.Loopback;
+
+
+
+//Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp );
+//IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("192.168.0.1"), 1234);
+
+//IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("192.168.0.1"), 1234);
+//socket.Bind(endPoint);
+//socket.Listen(1000); //nombre de connexion simultannées
+//socket.Accept(); // Bloque jusqu'a qu une connexion soit etablie
+
+
+try
+{
+    IPAddress ip_any = IPAddress.Any;
+
+    Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+    IPEndPoint endPoint = new IPEndPoint(ip_any, 2345);
+
+    socket.Bind(endPoint);
+    socket.Listen();
+    var clientSocket = socket.Accept();
+    if (clientSocket.RemoteEndPoint is not null)
+    {
+        System.Console.WriteLine(
+            "client connecté depuis :" + clientSocket.RemoteEndPoint.ToString()
+        );
+
+        while (true)
+        {
+            byte[] buffer = new byte[128];
+            int nbROctets = clientSocket.Receive(buffer);
+            System.Console.WriteLine(
+                "Message recu :" + Encoding.UTF8.GetString(buffer, 0, nbROctets)
+            );
+        }
+    }
+}
+catch (Exception ex)
+{
+    System.Console.WriteLine("une erreur est survenue");
+}
---
+++ client/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+IPEndPoint endPoint = new IPEndPoint(IPAddress.Loopback, 2345);
+socket.Connect(endPoint);
+
+while (true)
+{
+    string message = Console.ReadLine();
+
+    if (message == "q")
+    {
+        break;
+    }
+
+    var buffer = Encoding.UTF8.GetBytes(message);
+    socket.Send(buffer);
+}
